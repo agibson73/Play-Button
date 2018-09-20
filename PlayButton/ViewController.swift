c97b0349efcdcaf4b55b925d07bb8329cc9e6268
Alex Gibson <oakmonttech@gmail.com>
2018-09-20 11:36:19 -0400

    Initial Commit

---
+++ PlayButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PlayButton
+//
+//  Created by Alex Gibson on 9/20/18.
+//  Copyright © 2018 Alex Gibson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
